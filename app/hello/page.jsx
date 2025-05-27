81aff6f77f601fe45f54219a301a1eada39bbf71
nguyendevweb123 <nguyenunderground@gmail.com>
2025-05-27 14:08:20 +0700

    hihi

--- app/hello/page.jsx
+++ app/hello/page.jsx
@@ -2,7 +2,7 @@
 import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import VerificationCodeBox from '@/app/Components/verificationComponent/VerificationCodeBox';
-
+import ProductPage from '../product/page'
 const HelloPage = () => {
     const [verified, setVerified] = useState(false);
     const router = useRouter();
@@ -20,8 +20,10 @@ const HelloPage = () => {
 
     return (
         <div style={{ padding: '20px' }}>
-            <h1>🎉Hello!</h1>
+            <h1>🎉Hello!</h1>   
+            {/* <ProductPage></ProductPage>         */}
         </div>
+        
     );
 };
 
